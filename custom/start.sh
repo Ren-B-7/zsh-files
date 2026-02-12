8aafacf2372cdf31f0cb5f496a1297baaa071deb
Ren B <Renier52147@gmail.com>
2026-02-12 12:47:12 +0200

    Delete custom directory

--- custom/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-SCRIPT_DIR="$HOME/.custom_shell_scripts"
-
-source "$SCRIPT_DIR/FullUpgrade.sh"
-source "$SCRIPT_DIR/nvims_find.sh"
-source "$SCRIPT_DIR/set_ufw_permissions.sh"
-source "$SCRIPT_DIR/exports.sh"
-source "$SCRIPT_DIR/aliases_set.sh"
